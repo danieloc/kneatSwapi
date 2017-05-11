58b56f98b35c5909d2de8ea208d55657f0d5cbd8
Daniel O'Connor <daniel.o.c.03@gmail.com>
2017-05-11 22:56:27 +0100

    Improve unit tests
    Increase mocha timeout to allow promise to resolve.
    Add chai to allow 'expect'

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@
 var request = require('supertest');
 var assert = require('assert');
 var swapiLib = require('../swapiLib');
+var chai = require('chai');
+var expect = chai.expect;
 
 //Check to see that the Database is functioning
 describe('GET DEATH STAR INFO', function() {
@@ -32,27 +34,31 @@ describe('CHECK CALCULATIONS', function () {
     it('Check - \'getAmountOfHoursBeforeResupplies\' Function - \'2 weeks\'', function (done) {
         //Function should return '2' as 24 goes into 49 twice.
         var numberOfStops = swapiLib.getAmountOfHoursBeforeResupplies('2 weeks');
-        assert.equal(2*7*24, numberOfStops);
+        assert.equal(2 * 7 * 24, numberOfStops);
         return done();
     });
     it('Check - \'getAmountOfHoursBeforeResupplies\' Function - \'1 year\'', function (done) {
         var numberOfStops = swapiLib.getAmountOfHoursBeforeResupplies('1 year');
-        assert.equal(Math.round(24*365.2422), numberOfStops);
+        assert.equal(Math.round(24 * 365.2422), numberOfStops);
         return done();
     });
-    /*it('Check - \'makeStarshipRequests\' Function - [1,2,3]', function (done) {
-        //Checking
-        swapiLib.makeStarshipRequests([1,2,3]).then(function (starshipList) {
-                assert.equal(30, starshipList.length);
-                return done();
+});
+describe('MAKE ASYNCHRONOUS STARSHIP REQUESTS', function () {
+    it('Check - \'makeStarshipRequests\' Function - [1,2,3]', function () {
+        //Request Pages 1,2,3 - 10 Objects each.
+        //Pages 4,5 and is then called implicitly in the makeStarshipRequest function
+        //Page 4 has 7 objects making a total of 37.
+         return swapiLib.makeStarshipRequests([1,2,3]).then(function (starshipList) {
+             expect(starshipList.length).to.equal(37);
+         });
+    });
+    it('Check - \'makeStarshipRequests\' Function - [2,3]', function () {
+        //Request pages 2 and 3. 10 Objects each.
+        //Since neither pages 2 or 3 return a 404 - pages 4 and 5 are called
+        //Page 4 returns 7 objects bringing the total to 27 objects.
+        swapiLib.makeStarshipRequests([2,3]).then(function (starshipList) {
+            expect(starshipList.length).to.equal(27);
         });
     });
-    it('Check - \'makeStarshipRequests\' Function - [2,3]', function (done) {
-        //Checking
-        swapiLib.makeStarshipRequests([2,3]).then(function (starshipList) {
-            assert.equal(30, starshipList.length);
-            return done();
-        });
-    });*/
 });
 
